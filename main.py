9ef50e5a880e04c41586948b52483245c504fa84
Jose C <scadenao@unal.edu.co>
2023-05-14 09:41:07 -0500

    Set resolution, added display and grid

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import pygame
+from random import randint
+from copy import deepcopy
 
-print("Intento del juego este qliao")
+#Technical specifications of the program, display resolution, tile size and framerate
 
-print('First attemp to code from the machine')
+RES = WIDTH, HEIGHT = 1280, 720
+TILE = 50
+FPS = 15
+
+#Initial pygame function and main loop
+
+pygame.init()
+surface = pygame.display.set_mode(RES)
+clock = pygame.time.Clock()
+
+while True:
+
+    surface.fill(pygame.Color('black'))
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+          exit()
+
+    #Grid implementation
+
+    [pygame.draw.line(surface, pygame.Color('dimgray'), (x, 0), (x, HEIGHT)) for x in range (0, WIDTH, TILE)]
+    [pygame.draw.line(surface, pygame.Color('dimgray'), (0, y), (WIDTH, y)) for y in range (0, HEIGHT, TILE)]
+
+    pygame.display.flip()
+    clock.tick(FPS)
