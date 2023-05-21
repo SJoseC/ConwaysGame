e9f4a7fec5736ee9f99852048776f3a10c3545a9
Jose C <scadenao@unal.edu.co>
2023-05-20 22:57:47 -0500

    "Added icon and window title. Grid improved"

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pygame
 from random import randint
 from copy import deepcopy
 
-#Technical specifications of the program, display resolution, tile size and framerate
+#Technical specifications of the program, display resolution, tile size and framerate, window title and icon
 
+pygame.display.set_caption("Conway's Game of Life by SJoseC")
+ICON = pygame.image.load('conway_icon.jpg')
+pygame.display.set_icon(ICON)
 RES = WIDTH, HEIGHT = 1280, 720
-TILE = 50
+TILE = 5
+W, H= WIDTH // TILE, HEIGHT // TILE
 FPS = 15
 
 #Initial pygame function and main loop
@@ -13,6 +17,9 @@ FPS = 15
 pygame.init()
 surface = pygame.display.set_mode(RES)
 clock = pygame.time.Clock()
+
+NEXT_FIELD = [[0 for i in range(W)] for j in range(H)]
+CURRENT_FIELD = [[randint(0,1) for i in range (W)] for i in range (H)]
 
 while True:
 
@@ -23,8 +30,8 @@ while True:
 
     #Grid implementation
 
-    [pygame.draw.line(surface, pygame.Color('dimgray'), (x, 0), (x, HEIGHT)) for x in range (0, WIDTH, TILE)]
-    [pygame.draw.line(surface, pygame.Color('dimgray'), (0, y), (WIDTH, y)) for y in range (0, HEIGHT, TILE)]
+    [pygame.draw.line(surface, pygame.Color(10,10,10), (x, 0), (x, HEIGHT)) for x in range (0, WIDTH, TILE)]
+    [pygame.draw.line(surface, pygame.Color(10,10,10), (0, y), (WIDTH, y)) for y in range (0, HEIGHT, TILE)]
 
     pygame.display.flip()
     clock.tick(FPS)
